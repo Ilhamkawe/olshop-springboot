9380c34eb72d4c6b93f0137871ee0f04ed5c28b4
RicoBasyar <RicoBasyar@users.noreply.github.com>
2023-09-24 23:41:26 +0700

    @Entity

---
+++ src/main/java/com/olshop/olshop/entity/ProductGalleriesEntity.java
@@ -0,0 +1,39 @@
+package com.olshop.olshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "product_galleries")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductGalleriesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "product_id")
+    private int productID;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
