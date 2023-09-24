8a19ffcd2ae517b57049b8102cf3e1d3365fe3c3
ilhamKawe <kawekaweha123@gmail.com>
2023-09-24 22:46:29 +0700

    Add JWT Auth Filter

--- src/main/java/com/olshop/olshop/repository/UserRepository.java
+++ src/main/java/com/olshop/olshop/repository/UserRepository.java
@@ -1,4 +1,23 @@
 package com.olshop.olshop.repository;
 
-public class UserRepository {
+
+import com.olshop.olshop.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    @Query(value = "SELECT u FROM UserEntity  u WHERE u.email = :email")
+    UserEntity getUserByEmail(@Param("email") String email);
+
+
 }
