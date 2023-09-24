46db4fc91421f97518e86091e3f74c240e04489a
RicoBasyar <RicoBasyar@users.noreply.github.com>
2023-09-24 22:39:18 +0700

    @EntityTransaction

---
+++ src/main/java/com/olshop/olshop/entity/TransactionEntity.java
@@ -0,0 +1,53 @@
+package com.olshop.olshop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "user_id")
+    private int userID;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name= "total_price")
+    private int totalPrice;
+
+    @Column(name = "total_shipping")
+    private int totalShipping;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
